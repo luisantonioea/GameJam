eb5df7cc06255ac6742fc5a15c372c307a6b5741
kurobih <lantonioespinoza10@gmail.com>
2026-03-08 15:15:52 -0500

    Mejorandolo visualmente el formulario

--- Models/Jugador.cs
+++ Models/Jugador.cs
@@ -1,13 +1,18 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 
-namespace GameJamMVC.Models
+namespace GameJam.Models
 {
     public class Jugador
     {
+        [Required(ErrorMessage = "El nombre es obligatorio")]
         public string? Nombre { get; set; }
 
+        [Required(ErrorMessage = "Debe ingresar una fecha")]
+        [DataType(DataType.Date)]
         public DateTime FechaNacimiento { get; set; }
 
+        [Required(ErrorMessage = "Ingrese al menos un hobby")]
         public string? Hobbies { get; set; }
 
         public bool PagoMatricula { get; set; }
